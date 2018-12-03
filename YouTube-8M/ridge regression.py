bc27855ffbd9ab1ba1ab4a7aa812a5c464994e36
llamore <41164648+llamore@users.noreply.github.com>
2018-12-02 20:19:24 -0800

    Update ridge regression.py

--- YouTube-8M/ridge regression.py
+++ YouTube-8M/ridge regression.py
@@ -1,31 +1,97 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 29 20:37:12 2018
-
-@author: Lance
+Created on Thu Nov 29 21:42:51 2018
 """
 
+from xgboost import XGBRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn import svm
 from sklearn.linear_model import Ridge
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import AdaBoostRegressor
 
-df = pd.read_csv('video_stats.csv', encoding = "ISO-8859-1")
-df['PublishedAt'] = pd.to_datetime(df['publishedAt'], errors='coerce')
-df = df.dropna()
-df['logViews'] = np.log(df['viewCount']+1)
 
-channels = {}
-for _, video in df.iterrows():
-    channel_id = video['channel Id']
-    if not channel_id in channels:
-        channels[channel_id] = [video]
-    else:
-        channels[channel_id].append(video)
-for channel_id in channels:
-    channels[channel_id] = sorted(channels[channel_id], key=lambda v: v['PublishedAt'])
+def svm_regressor(X_train, y_train, X_test, y_test):
+    clf = svm.SVR()
+    clf.fit(X_train, y_train)
+    
+    
+    svm_pred = clf.predict(X_test)
+    
+    plot_df = pd.DataFrame(svm_pred, columns=['Predictions'])
+    plot_df['True'] = np.array(y_test)
+    sns.regplot(x='Predictions', y='True', data=plot_df)
+        
+    
+    print (mean_squared_error(y_test,svm_pred))
+
+def Ridge_regressor(X_train, y_train, X_test, y_test):
+    rid = linear_model.Ridge(alpha=1.0)
+    rid.fit(X_train, y_train)
+    
+    
+    ridge_pred = rid.predict(X_test)
+    
+    plot_df = pd.DataFrame(ridge_pred, columns=['Predictions'])
+    plot_df['True'] = np.array(y_test)
+    sns.regplot(x='Predictions', y='True', data=plot_df)
+        
+     
+    print (mean_squared_error(y_test,ridge_pred))
+    
+
+def XGB_regressor(X_train, y_train, X_test, y_test):
+    reg = GradientBoostingRegressor()
+    reg.fit(X_train, y_train)
+    reg_pred = reg.predict(X_test)
+    
+    plot_df = pd.DataFrame(reg_pred, columns=['Predictions'])
+    plot_df['True'] = np.array(y_test)
+    sns.regplot(x='Predictions', y='True', data=plot_df)
+    
+    print (mean_squared_error(y_test,reg.predict(X_test)))
+    
+def Random_forest(X_train, y_train, X_test, y_test):
+    reg = RandomForestRegressor()
+    reg.fit(X_train, y_train)
+    reg_pred = reg.predict(X_test)
+    
+    plot_df = pd.DataFrame(reg_pred, columns=['Predictions'])
+    plot_df['True'] = np.array(y_test)
+    sns.regplot(x='Predictions', y='True', data=plot_df)
+    
+    print (mean_squared_error(y_test,reg.predict(X_test)))
+    
+def Adaboost_regression(X_train, y_train, X_test, y_test):
+    reg = AdaBoostRegressor()
+    reg.fit(X_train, y_train)
+    reg_pred = reg.predict(X_test)
+    
+    plot_df = pd.DataFrame(reg_pred, columns=['Predictions'])
+    plot_df['True'] = np.array(y_test)
+    sns.regplot(x='Predictions', y='True', data=plot_df)
+    
+    print (mean_squared_error(y_test,reg.predict(X_test)))
+
+def linear_regressor(X_train, y_train, X_test, y_test):
+    reg = linear_model.Lasso()
+    reg.fit(X_train, y_train)
+    reg_pred = reg.predict(X_test)
+    
+    plot_df = pd.DataFrame(reg_pred, columns = ['Predictions'])
+    plot_df['True'] = np.array(y_test)
+    sns.regplot(x='Predictions', y='True', data=plot_df)
+    
+    print (mean_squared_error(y_test,reg_pred))
+
+
     
 def prediction_answer_and_feature_extractor(channel, n):
     """
@@ -51,6 +117,21 @@ def prediction_answer_and_feature_extractor(channel, n):
         feature_names += [name + "%d"%(i+1) for name in video_stat_names]
     return (correct_prediction, dict(zip(feature_names, feature_values)))
 
+
+df = pd.read_csv('video_stats.csv', encoding = "ISO-8859-1")
+df['PublishedAt'] = pd.to_datetime(df['publishedAt'], errors='coerce')
+df = df.dropna()
+df['logViews'] = np.log(df['viewCount']+1)
+
+channels = {}
+for _, video in df.iterrows():
+    channel_id = video['channel Id']
+    if not channel_id in channels:
+        channels[channel_id] = [video]
+    else:
+        channels[channel_id].append(video)
+for channel_id in channels:
+    channels[channel_id] = sorted(channels[channel_id], key=lambda v: v['PublishedAt'])
 prolific_channels = {k:v for k, v in channels.items() if 11 < len(v)}
 
 data = []
@@ -60,9 +141,7 @@ for channel_id, vids in prolific_channels.items():
     vid_features['trueViewCount'] = answer
     data.append(vid_features)
     
-from xgboost import XGBRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import train_test_split
+
 
 features = ['likeCount', 'dislikeCount', 'viewCount', 
                         'favoriteCount', 'commentCount']
@@ -70,27 +149,29 @@ real_features = [name+'-%d'%i for i in range(1,11) for name in features]
 df = pd.DataFrame(data)
 df = df.dropna()
 X = df[real_features]
+
 y = df['trueViewCount']
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
+svm_regressor(X_train, y_train, X_test, y_test)
+Ridge_regressor(X_train, y_train, X_test, y_test)
+XGB_regressor(X_train, y_train, X_test, y_test)
+Random_forest(X_train, y_train, X_test, y_test)
+linear_regressor(X_train, y_train, X_test, y_test)
 
-n_samples, n_features = 10, 5
-np.random.seed(0)
-y = np.random.randn(n_samples)
-X = np.random.randn(n_samples, n_features)
-
-clf = Ridge(alpha=1.0)
-clf.fit(X_train, y_train)
+correlation = X.corr()
 
 
-ridge_pred = clf.predict(X_test)
 
-plot_df = pd.DataFrame(ridge_pred, columns=['Predictions'])
-plot_df['True'] = np.array(y_test)
-sns.regplot(x='Predictions', y='True', data=plot_df)
-    
- 
-print (mean_squared_error(y_test,ridge_pred))
+
+
+
+
+
+
+
 
 
 
